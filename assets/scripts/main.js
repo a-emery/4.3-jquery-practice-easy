e387ca40db475f26d0d0dda07a9dccabc1bd69fb
Aaron Emery <aemery14@gmail.com>
2015-09-17 08:48:18 -0400

    Finished up

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
-  $('.bottom-level-list').slideUp();
   $('h3').click(function() {
     $('.bottom-level-list').slideUp();
-    if (!$(this).next().is(':visible'))
-    {
-    $(this).next().slideDown();
-  }
+    if (!$(this).next().is(':visible')) {
+      $(this).next().slideDown();
+    }
   });
 });
